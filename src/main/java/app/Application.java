f15240e18d20f5db3298eaef58159e0beac0fcec
Skyler <skylerjaneclark@gmail.com>
2019-02-28 21:51:02 -0500

    sets up spark project

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,9 @@
+package app;
+
+import static spark.Spark.*;
+
+public class Application {
+    public static void main(String[] args) {
+        get("/", (req, res) -> "h--hewwo??? owo;;; mistew posteman???");
+    }
+}
