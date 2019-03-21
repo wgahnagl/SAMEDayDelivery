b290e638e054a7f8da220048e52dec098f2533f8
Evan Rysdam <evan@rysdam.org>
2019-03-20 20:27:07 -0400

    Cleaned up the DBLiason class a bit.

--- src/main/java/app/DBLiason.java
+++ src/main/java/app/DBLiason.java
@@ -10,13 +10,34 @@ import java.sql.Statement;
  */
 public class DBLiason {
     // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "org.h2.Driver";
-    static final String DB_URL = "jdbc:h2:./db";
+    private static final String JDBC_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:./db";
 
-    //  Database credentials
-    static final String USER = "sa";
-    static final String PASS = "pa";
+    // Database credentials
+    private static final String USER = "sa";
+    private static final String PASS = "pa";
 
+    // Rope that ties Java to SQL
+    private static final Connection connection;
+    private static final Statement statement;
+
+    // Initialization of the above two variables (a stupid hack to work around the fact
+    // that they variables are final, yet their initialization can throw an Exception)
+    static {
+        Connection connectionTemp = null;
+        Statement statementTemp = null;
+        try {
+            connectionTemp = DriverManager.getConnection(DB_URL, USER, PASS);
+            statementTemp = connectionTemp.createStatement();
+        } catch(SQLException sqle) {
+            System.out.println("FATAL ERROR: Encountered SQLException while establishing connection to the database");
+            System.out.println("Stack trace:");
+            sqle.printStackTrace();
+            System.exit(-1);
+        }
+        connection = connectionTemp;
+        statement = statementTemp;
+    }
 
     /* DB SETUP METHODS
        These methods should not really ever be called during normal program execution.
@@ -27,7 +48,7 @@ public class DBLiason {
        The setupDB() method calls in sequence each of the methods below it, which
        should all follow this format:
 
-           private static void setup<TableName>Table (Statement stmt) throws SQLException {
+           private static void setup<TableName>Table (Statement statement) throws SQLException {
                <Code to delete the table>
                <Code to recreate the table>
                <Code to populate the table with our sample data>
@@ -35,20 +56,11 @@ public class DBLiason {
      */
 
     public static void setupDB() {
-        System.out.println("Setting up the CB ");
-
-        Connection conn = null;
-        Statement stmt = null;
-        String sql;
+        System.out.println("Setting up the DB ");
 
         try {
-            System.out.println("Connecting to the database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            stmt = conn.createStatement();
-
             /* Systematically destroy and then re-create every table in the database */
-
-            setupPackageTable(stmt);
+            setupPackageTable();
 
         } catch(SQLException sqle) {
             sqle.printStackTrace();
@@ -57,9 +69,9 @@ public class DBLiason {
         System.out.println("Finished creating the database");
     }
 
-    private static void setupPackageTable(Statement stmt) throws SQLException {
-        stmt.execute("drop table package;");
-        stmt.execute("create table package (" +
+    private static void setupPackageTable() throws SQLException {
+        statement.execute("drop table package;");
+        statement.execute("create table package (" +
                 "ID int primary key," +
                 "shiptime timestamp" +
                 ");");
@@ -71,7 +83,7 @@ public class DBLiason {
      * the graphical UI and the command-line UI.
      */
 
-    
+
 
 
     /* Main method for testing only */
