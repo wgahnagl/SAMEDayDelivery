c0e33b953ff90da9057f32c0b070672f001a0a86
Skyler <skylerjaneclark@gmail.com>
2019-03-21 16:53:26 -0400

    adds BOILERPLATE

--- src/main/java/app/ui/WebServer.java
+++ src/main/java/app/ui/WebServer.java
@@ -7,6 +7,11 @@ import spark.TemplateEngine;
 public class WebServer {
 
     public static final String HOME_URL = "/";
+    public static final String SIGNIN_URL = "/signin";
+    public static final String SIGNUP_URL = "/signup";
+    public static final String TRACKING_URL = "/tracking";
+    public static final String ACCOUNT_URL = "/account";
+    public static final String FORGOT_PASSWORD_URL = "/password_recover";
 
     private final TemplateEngine templateEngine;
 
@@ -18,6 +23,11 @@ public class WebServer {
     public void initialize() {
         staticFileLocation("/public");
         get(HOME_URL, new GetHomeRoute(templateEngine));
+        get(SIGNIN_URL, new GetSignInRoute(templateEngine));
+        get(SIGNUP_URL, new GetSignUpRoute(templateEngine));
+        get(TRACKING_URL, new GetTrackingRoute(templateEngine));
+        get(ACCOUNT_URL, new GetAccountRoute(templateEngine));
+        get(FORGOT_PASSWORD_URL, new GetPasswordRecoverRoute(templateEngine));
     }
 
 }
