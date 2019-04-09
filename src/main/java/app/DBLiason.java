79d55bb6a137a787e298b297dea2e839cdc892b8
Evan Rysdam <evan@rysdam.org>
2019-04-09 18:39:36 -0400

    Added prettifyResultSet() and update prettyPackageList() and prettyUserList() to use it. Also added untested methods for adding customers and linking addresses.

--- src/main/java/app/DBLiason.java
+++ src/main/java/app/DBLiason.java
@@ -191,7 +191,7 @@ public class DBLiason {
 
     private static void setupCustomerHasBankAccountTable() throws SQLException {
         statement.execute("drop table customerHasBankAccount if exists");
-        statement.execute("create table customerBankAccount(" +
+        statement.execute("create table customerHasBankAccount(" +
                 "customer_id int, " +
                 "acct_number varchar(255)," +
 
@@ -231,13 +231,14 @@ public class DBLiason {
         return value.replaceAll("'", "''");
     }
 
-    // TODO: Rename this method to "formatCommand" since there's no reason it can't
-    // be that general. Then change all the usages to match.  (It is already used the new
-    // way in getCustomerByAddress). Also, switch the arguments to be consistent
-    // with the String.format() frame.
     private static String formatInputRow(String valueList, String formatString) {
         // Apply a format string like ("%3, %2, %1, '%4', %5")
         // to a list of values like "pie, 32, 42, David Smith, hello"
+
+        // TODO: Rename this method to "formatCommand" since there's no reason it can't
+        // be that general. Then change all the usages to match.  (It is already used the new
+        // way in getCustomerByAddress). Also, switch the arguments to be consistent
+        // with the String.format() frame.
 
         String[] values = valueList.split(",");
         String result = formatString;
@@ -288,10 +289,11 @@ public class DBLiason {
         populateTableFromCSV(tablename, filename, "");
     }
 
-    private static void prettifyResultSet(ResultSet rs, String format) throws SQLException {
+    private static ArrayList<String> prettifyResultSet( String format, ResultSet rs ) throws SQLException {
         /* UNDER CONSTRUCTION */
 
         ArrayList<String> formatted = new ArrayList<>();
+        rs.first();
 
         while(rs.next()) {
             // Start with an empty row and build it up one char at a time
@@ -300,29 +302,67 @@ public class DBLiason {
             String thisRow = "";
             String formatCopy = format;
 
-            while(!formatCopy.equals("")) {
-                switch(formatCopy.charAt(0)) {
+            while( !formatCopy.equals("") ) {
+                System.out.println( thisRow + " | " + formatCopy);
+
+                switch( formatCopy.charAt(0) ) {
 
                     // For % signs, insert a value from the table
                     case '%':
-                        int upTo = formatCopy.indexOf(')');
+                        int upTo = formatCopy.indexOf(')'); // Technically this is a bug as "MAX(ID)" is a valid column name
                         String[] typeVar = formatCopy.substring(2, upTo).split(",");
-                        formatCopy = formatCopy.substring(upTo);
+                        formatCopy = formatCopy.substring(upTo+1);
 
                         switch(typeVar[0].toLowerCase()) {
                             case "int":
                             case "d":
                                 thisRow += rs.getInt(typeVar[1]);
+                                break;
+
+                            case "str":
+                            case "string":
+                            case "s":
+                                thisRow += rs.getString(typeVar[1]);
+                                break;
+
+                            case "timestamp":
+                                thisRow += rs.getTimestamp(typeVar[1]).toString();
+                                break;
+
+                            default:
+                                throw new RuntimeException("Error inside prettifyResultSet(): Unrecognized type <" + typeVar[0] + ">");
                         }
+                        break;
 
                     // Copy the next character literally, no matter what it is
                     case '\\':
                         thisRow += formatCopy.charAt(1);
                         formatCopy = formatCopy.substring(2);
                         break;
+
+                    default:
+                        thisRow += formatCopy.charAt(0);
+                        formatCopy = formatCopy.substring(1);
+                        break;
                 }
             }
+
+            formatted.add(thisRow);
         }
+
+        return formatted;
+    }
+
+    private static String asLines( ArrayList<String> lines ) {
+        if(lines.isEmpty()) return "";
+
+        String result = "";
+        for( String l : lines ) {
+            result += "\n";
+            result += l;
+        }
+
+        return result.substring(1);
     }
 
 
@@ -332,8 +372,7 @@ public class DBLiason {
         return maxIdResult.getInt("MAX(ID)");
     }
         
-    private static void addCustomerByInfo( String lastName, String firstName, String email )
-                                           throws SQLException {
+    private static void addCustomerByInfo( String lastName, String firstName, String email ) throws SQLException {
         String valuesFmt = "%d,%s,%s,%s"; // Put parameters into a String
         String rowFmt = "%1, '%2', '%3', '%4', null, null, null, null, null, null";
         String insertCmdFmt = "insert into customer values (%s);";
@@ -347,9 +386,7 @@ public class DBLiason {
         statement.execute( insertCmd );
     }
 
-    private static void addCustomerByAddr( String addr_line1, String addr_line2,String city,
-                                           String province, String zipcode, String country )
-                                           throws SQLException {
+    private static void addCustomerByAddr( String addr_line1, String addr_line2,String city, String province, String zipcode, String country ) throws SQLException {
         String valuesFmt = "%d,%s,%s,%s,%s,%s,%s";
         String rowFmt = "%1, null, null, null, '%2', '%3', '%4', '%5', '%6', '%7'";
         String insertCmdFmt = "insert into customer values (%s);";
@@ -376,9 +413,7 @@ public class DBLiason {
         return result.getInt("ID");
     }   
     
-    private static int getCustomerByAddr( String addr_line1, String addr_line2, String city,
-                                          String province, String zipcode, String country )
-                                          throws SQLException {
+    private static int getCustomerByAddr( String addr_line1, String addr_line2, String city, String province, String zipcode, String country ) throws SQLException {
         String valuesFmt = "%s,%s,%s,%s,%s,%s";
         String cmdFmt = "select from customer where " +
             "addr_line1 = '%1' and " +
@@ -398,10 +433,7 @@ public class DBLiason {
         return result.getInt("ID");
     }
 
-    private static boolean linkAddress( String email, String addr_line1, String addr_line2,
-                                        String city, String province, String zipcode,
-                                        String country )
-                                        throws SQLException {
+    private static boolean linkAddress( String email, String addr_line1, String addr_line2, String city, String province, String zipcode, String country ) throws SQLException {
         // TODO (Or maybe it can be left alone)
         // This method is written with the admittedly naive assumption that the customer
         // was honest (and not mistaken) about their address. If it finds a customer A with
@@ -459,29 +491,18 @@ public class DBLiason {
     }
 
 
-
-
     // Todo: Generalize prettyPackageList and prettyCustomerList into prettyResults(ResultSet result, String format)
     // which will take a string like "Package: %(d,id). Ship time: %(timestamp,ship_time)".
 
     public static String prettyPackageList() {
         // Return a String of a newline-separated list of all the packages in the DB
-        // (Consider generalizing this method into a prettyTable method where you pass
-        //  it something like ("Package [%d] from %s to %s", "ID", "fromCustomer", "toCustomer"))
+
+        ArrayList<String> prettified;
 
         try {
-            String result = "";
             ResultSet packages = statement.executeQuery("select ID, ship_timestamp from package");
-
-            while(packages.next()) {
-                int nextID = packages.getInt("ID");
-                String nextShipTime = packages.getTimestamp("ship_timestamp").toString();
-
-                result += String.format("Package %d. Ship time: %s\n", nextID, nextShipTime);
-            }
-
-            return result;
-
+            prettified = prettifyResultSet( "PackageID #%(d,ID) (shipped at #%(timestamp,ship_timestamp))", packages );
+            return asLines(prettified);
         } catch(SQLException sqle) {
             sqle.printStackTrace();
             return "<SQL error>";
@@ -491,19 +512,12 @@ public class DBLiason {
     public static String prettyCustomerList() {
         // Return a String of a newline-separated list of all the customers in the DB
 
+        ArrayList<String> prettified;
+
         try {
-            String result = "";
-            ResultSet customers = statement.executeQuery("select ID, last_name, first_name from customer");
-
-            while(customers.next()) {
-                int nextID = customers.getInt("ID");
-                String nextName = customers.getString("first_name") + " " + customers.getString("last_name");
-
-                result += String.format("Customer #%d (%s)\n", nextID, nextName);
-            }
-
-            return result;
-
+            ResultSet packages = statement.executeQuery("select ID, last_name, first_name from customer");
+            prettified = prettifyResultSet( "Customer #%(d,ID): %(s,first_name) %(s,last_name)", packages );
+            return asLines(prettified);
         } catch(SQLException sqle) {
             sqle.printStackTrace();
             return "<SQL error>";
